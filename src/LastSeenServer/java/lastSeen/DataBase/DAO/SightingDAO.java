21dbc6a3fc71a0c4120357552f57e20b345674bc
yoavsusan <yoavs988@gmail.com>
2015-11-24 18:22:01 +0200

    SightingDAO (not final)

---
+++ src/LastSeenServer/java/lastSeen/DataBase/DAO/SightingDAO.java
@@ -0,0 +1,45 @@
+package lastSeen.DataBase.DAO;
+
+import lastSeen.DataBase.dbObjects.Sighting;
+import lastSeen.DataBase.hibernate.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+/**
+ * Created by yoavsusan on 21/11/2015.
+ */
+public class SightingDAO {
+
+    private static String findAll = "Sighting.findAll";
+
+    public static List<Sighting> findAll() {
+        System.out.println(SightingDAO.class + ": finaAll : Start");
+        List<Sighting> sightings;
+        Session session;
+        Transaction transaction;
+        session = HibernateUtil.getSession();
+        transaction = session.beginTransaction();
+        try {
+            Query query;
+            query = session.getNamedQuery("Contact.findAll");
+            sightings = HibernateUtil.listFrom(query);
+
+        } catch (final RuntimeException e) {
+            System.err.println("RuntimeException: " + e.getMessage());
+            transaction.rollback();
+            throw e;
+        } finally {
+            try {
+                session.flush();
+                session.close();
+            } catch (final Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println(SightingDAO.class + ": finaAll : End");
+        return sightings;
+    }
+}
