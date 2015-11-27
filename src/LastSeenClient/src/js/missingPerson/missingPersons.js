2907af4f32e9c02e0cd0c429a3c0c45125dc0bb9
Itzik Kabessa <itzik.kabessa@hp.com>
2015-11-28 00:29:58 +0200

    start of application UI main page

---
+++ src/LastSeenClient/src/js/missingPerson/missingPersons.js
@@ -0,0 +1,7 @@
+var missingPersons = angular.module('missingPersons',[]);
+
+missingPersons.controller('missingPersonsController', ['$scope', '$location', function($scope, $location){
+
+    $scope.message = "MissingPersonsController";
+
+}]);
