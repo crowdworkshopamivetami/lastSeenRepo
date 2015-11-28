9ec79757696ee132ab6fd7c4484f8f227b10f9f8
Itzik Kabessa <itzik.kabessa@hp.com>
2015-11-28 13:14:27 +0200

    sync with yoav

---
+++ src/LastSeenServer/java/lastSeen/rest/services/missingPersonRest.java
@@ -0,0 +1,34 @@
+package lastSeen.rest.services;
+
+import lastSeen.DataBase.DAO.MissingPersonDAO;
+import lastSeen.rest.services.result.RestResult;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by yoavsusan on 11/27/2015.
+ */
+@Path("/missingPersonRest")
+public class missingPersonRest implements  RestService{
+    @Context
+    private HttpServletRequest httpServletRequest;
+    @Context
+    private HttpServletRequest httpServletResponse;
+
+    @Override
+    public HttpServletRequest getHttpServletRequest() {
+        return httpServletRequest;
+    }
+
+    @GET
+    @Path("/getAllMissingPersons")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RestResult getAllMissingPersons() {
+        return new RestResult(true, "success",  MissingPersonDAO.findAll());
+    }
+}
